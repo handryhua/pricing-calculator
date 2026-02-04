9fec430495082a227d705c782e94d0670980b84b
handryhua <58896496+handryhua@users.noreply.github.com>
2026-02-04 21:10:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,39 @@ const CATEGORY_LABEL = {
 
 // ================== PRODUCT MASTER ==================
 let products = [
-  { sku:"8085", category:"BPM", displayName:"BPM HEM-7120", price:408000, specialDiscount:0.20, qty:0 },
-  { sku:"4682", category:"NEBU", displayName:"Nebu NE-C28", price:619500, specialDiscount:0.125, qty:0 },
-  { sku:"E245", category:"EFT", displayName:"EFT MC-246", price:56880, specialDiscount:0.058, qty:0 },
+  // ================= BCM =================
+  { sku:"8504", category:"BCM", displayName:"HN-289-EBK", price:227739, specialDiscount:0.0625, qty:0 },
+  { sku:"8091", category:"BCM", displayName:"HBF-214-AP", price:1229500, specialDiscount:0.1667, qty:0 },
+  { sku:"6830", category:"BCM", displayName:"HBF-375-AP", price:1641320, specialDiscount:0.11, qty:0 },
+
+  // ================= BPM =================
+  { sku:"E658", category:"BPM", displayName:"HEM-7142T2-AP2", price:412000, specialDiscount:0.20, qty:0 },
+  { sku:"8085", category:"BPM", displayName:"HEM-7120", price:408000, specialDiscount:0.20, qty:0 },
+  { sku:"B813", category:"BPM", displayName:"HEM-7156AAP", price:685000, specialDiscount:0.08, qty:0 },
+  { sku:"E250", category:"BPM", displayName:"HEM-7156-AP", price:577280, specialDiscount:0.08, qty:0 },
+  { sku:"D526", category:"BPM", displayName:"HEM-7156T-AP", price:619500, specialDiscount:0.08, qty:0 },
+  { sku:"D239", category:"BPM", displayName:"HEM-7156T-AAP", price:783000, specialDiscount:0.08, qty:0 },
+  { sku:"F140", category:"BPM", displayName:"HEM-7383T1-AP", price:1230000, specialDiscount:0.0625, qty:0 },
+  { sku:"E670", category:"BPM", displayName:"HEM-7157-AP3[JPN750]", price:702512, specialDiscount:0.10, qty:0 },
+  { sku:"E457", category:"BPM", displayName:"HEM-7530T-AP3", price:2492440, specialDiscount:0.11, qty:0 },
+
+  // ================= NEBU =================
+  { sku:"4682", category:"NEBU", displayName:"NE-C28", price:619500, specialDiscount:0.125, qty:0 },
+  { sku:"D252", category:"NEBU", displayName:"NE-C28-C1", price:619500, specialDiscount:0.125, qty:0 },
+  { sku:"B292", category:"NEBU", displayName:"NE-C101-AP", price:429250, specialDiscount:0.10, qty:0 },
+  { sku:"7727", category:"NEBU", displayName:"NE-C801S-AP", price:468500, specialDiscount:0.11, qty:0 },
+
+  // ================= EFT =================
+  { sku:"E245", category:"EFT", displayName:"MC-246-AP4", price:56880, specialDiscount:0.058, qty:0 },
+  { sku:"E297", category:"EFT", displayName:"MC-341-AP4", price:59940, specialDiscount:0.09, qty:0 },
+  { sku:"E089", category:"EFT", displayName:"MC-343F-AP4", price:78220, specialDiscount:0.047, qty:0 },
+  { sku:"F139", category:"EFT", displayName:"MC-F300-AP", price:452000, specialDiscount:0.09, qty:0 },
+  { sku:"8375", category:"EFT", displayName:"MC-523-AP", price:452153, specialDiscount:0.07, qty:0 },
+
+  // ================= TENS =================
+  { sku:"B735", category:"TENS", displayName:"HV-F013-AP4", price:445493, specialDiscount:0.14, qty:0 },
+
+  // ================= ACCESSORY =================
   { sku:"6865", category:"BPM", displayName:"Adaptor BPM", price:98011, specialDiscount:0.00, qty:0 }
 ];
 
